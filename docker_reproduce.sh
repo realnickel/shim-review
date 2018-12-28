b779216210b6815aabbf77d136c98466cfb8c311
Nikolai Kostrigin <nickel@altlinux.org>
2018-12-28 13:19:40 +0300

    docker_reproduce.sh: change rebuild artifacts destination directory

--- docker_reproduce.sh
+++ docker_reproduce.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BUILDDIR="build"
 
 ## (c)2018 Nikolai Kostrigin <nickel@basealt.ru>
 ## ALT container with hasher based reproducible build for shim binaries
@@ -7,16 +8,17 @@ docker build -t alt:sisyphus . -f Dockerfile
 #privileged is vital here due to isolation support by the kernel inside the container environment
 docker container run -it --privileged --user builder --workdir /home/builder/shim --name shim_rebuild alt:sisyphus /bin/bash -c "cd /home/builder/shim && gear-hsh -v --commit -- -v --lazy-cleanup --mountpoint=/proc --no-sisyphus-check=packager,gpg 2>&1 | tee log"
 
+mkdir $
 #build log
-docker container cp shim_rebuild:/home/builder/shim/log .
+docker container cp shim_rebuild:/home/builder/shim/log ./$BUILDDIR
 #package build RPM style spec file
-docker container cp shim_rebuild:/home/builder/shim/.gear/shim.spec .
+docker container cp shim_rebuild:/home/builder/shim/.gear/shim.spec ./$BUILDDIR
 #ia32 binary
-docker container cp shim_rebuild:/home/builder/hasher/chroot/usr/src/RPM/BUILD/shim-15/build-ia32/shimia32.efi .
+docker container cp shim_rebuild:/home/builder/hasher/chroot/usr/src/RPM/BUILD/shim-15/build-ia32/shimia32.efi ./$BUILDDIR
 #x64 binary
-docker container cp shim_rebuild:/home/builder/hasher/chroot/usr/src/RPM/BUILD/shim-15/build-x64/shimx64.efi .
+docker container cp shim_rebuild:/home/builder/hasher/chroot/usr/src/RPM/BUILD/shim-15/build-x64/shimx64.efi ./$BUILDDIR
 #sha256 hashes
-sha256sum ./shimia32.efi ./shimx64.efi > sha256sum.shim
+sha256sum ./shimia32.efi ./shimx64.efi > ./$BUILDDIR/sha256sum.shim
 
 docker container rm shim_rebuild
 
